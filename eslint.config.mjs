ea60a260bbb1fcbcbe83c5ed584e2f33b16269af
SlushEE0 <neeladem2009@gmail.com>
2025-09-13 16:44:29 -0700

    disabled eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,12 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname
+  baseDirectory: import.meta.url
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   ...compat.config({
+    extends: ['next'],
     rules: {
       // "no-console": "warn",
       "no-unused-vars": "warn",
@@ -19,15 +14,7 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-explicit-any": "warn",
     }
-  }),
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      parserOptions: {
-        project: "./tsconfig.json"
-      }
-    }
-  }
+  })
 ];
 
 export default eslintConfig;
